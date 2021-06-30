97abc245c445c752e3562f2eef6f9416ac48b781
Your Namechristophe-cpu <christophe-michaud1@orange.fr>
2021-06-30 17:01:37 +0200

    menu links ok

--- my-app/wild-formule1/my-app/src/Components/Navbar/Navbar.jsx
+++ my-app/wild-formule1/my-app/src/Components/Navbar/Navbar.jsx
@@ -6,6 +6,11 @@ function Navbar() {
     return (
         <div className="Navbar">
             <a href="/" ><img className="logo" src={logo} alt="logo F3"/></a>
+            <div className="navigation">
+                <a className="navigation-link" href="C:\Users\Utilisateur\Desktop\Nouveau dossier\photo-1508176694280-97e78b36d5fd.webp">LES VOITURES</a>
+                <a className="navigation-link" href="/">A PROPOS</a>
+                <a className="navigation-link" href="/">CONTACT</a>
+            </div>
         </div>
     )
 }
