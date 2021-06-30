525ebb74eb3e9266686db17dc3d990f1647b9926
Your Namechristophe-cpu <christophe-michaud1@orange.fr>
2021-07-01 00:14:04 +0200

    menu link ok

--- my-app/wild-formule1/my-app/src/Components/Navbar/Navbar.jsx
+++ my-app/wild-formule1/my-app/src/Components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar() {
         <div className="Navbar">
             <a href="/" ><img className="logo" src={logo} alt="logo F3"/></a>
             <div className="navigation">
-                <a className="navigation-link" href="C:\Users\Utilisateur\Desktop\Nouveau dossier\photo-1508176694280-97e78b36d5fd.webp">LES VOITURES</a>
+                <a className="navigation-link" href="file:///C:/Users/Utilisateur/Desktop/Nouveau%20dossier/photo-1491659015174-15b37116b4a4.webp">LES VOITURES</a>
                 <a className="navigation-link" href="/">A PROPOS</a>
                 <a className="navigation-link" href="/">CONTACT</a>
             </div>
