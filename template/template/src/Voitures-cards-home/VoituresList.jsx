9617d99636b5014c48a32fe0d367ec33e40f7529
Your Namechristophe-cpu <christophe-michaud1@orange.fr>
2021-07-02 00:02:11 +0200

    voiturelist

--- template/template/src/Voitures-cards-home/VoituresList.jsx
+++ template/template/src/Voitures-cards-home/VoituresList.jsx
@@ -7,17 +7,17 @@ function VoituresList() {
     const EMC2 ={
         name:"EMC²",
         image:"file:///C:/Users/Utilisateur/Desktop/Nouveau%20dossier/photo-1508176694280-97e78b36d5fd.webp"
-    }
+    };
 
     const STAND ={
         name:"Voiture au stand",
         image:"file:///C:/Users/Utilisateur/Desktop/Nouveau%20dossier/photo-1582854050148-651d87fa3319.webp"
-    }
+    };
     
     const RAY ={
         name:"Ray-bun",
         image:"file:///C:/Users/Utilisateur/Desktop/Nouveau%20dossier/photo-1508175911810-a4817cd3d7b4.webp"
-    }
+    };
 
     return (
         <div className="VoituresList">
